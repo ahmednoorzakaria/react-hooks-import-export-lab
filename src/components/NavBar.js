965b3852d64b2bbe6503d3cc4bb4392455b08101
ahmednoorzakaria <ahmednoor.zakaria@student.moringaschool.com>
2023-07-19 08:31:14 +0300

    mastered export import

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,6 @@
 import React from "react";
+import{username,city,image} from "../data/user"
+
 
 function NavBar() {
   return (
@@ -7,3 +9,5 @@ function NavBar() {
     </nav>
   );
 }
+
+export default NavBar;
